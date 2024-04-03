3d5ff57ae95f8d0a90568b1a994a77f14ce5a56a
Your Name <you@example.com>
2024-04-03 15:12:33 -0500

    Version code changed, App bundle signed and made, applicationId changed.

--- app/src/androidTest/java/com/example/timehack/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/timehack/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.timehack", appContext.getPackageName());
+        assertEquals("com.chillvibezz.timehack", appContext.getPackageName());
     }
 }
